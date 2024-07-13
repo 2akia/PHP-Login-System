241214bbd1be345a6d539893e9df6e469f2d5aa3
2akia <2akssultana@gmail.com>
2024-07-13 17:47:00 +0100

    Update login.php
    error on empty username and password

--- login.php
+++ login.php
@@ -3,4 +3,25 @@ session_start();
 
 include "db_conn.php";
 
-if (isset($_POST['uname']) && isset($_POST))
+if (isset($_POST['uname']) && isset($_POST)) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return data;
+    }
+}
+
+$uname = validate($_POST['uname']);
+$pass = validate($_POST['password']);
+
+if (empty($uname)) {
+    header ("Location: index.php?error=user Name is required");
+    exit();
+} else if (empty($pass)) {
+    header("Location: index.php?error=Password is required");
+    exit();
+}
+
+
